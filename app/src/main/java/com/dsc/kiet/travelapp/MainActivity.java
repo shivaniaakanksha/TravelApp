2854ae6454c0564cb90c540359ba19ccaf58027f
shivaniaakanksha <shivaniaakanksha@gmail.com>
2019-05-11 22:57:47 +0530

    login_page

--- app/src/main/java/com/dsc/kiet/travelapp/MainActivity.java
+++ app/src/main/java/com/dsc/kiet/travelapp/MainActivity.java
@@ -5,16 +5,29 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import gr.net.maroulis.library.EasySplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_btn = findViewById(R.id.login_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),check.class);
+                startActivity(intent);
+            }
+        });
 
 
 
